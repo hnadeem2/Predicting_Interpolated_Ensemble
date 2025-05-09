a0549173efb485ffc7b2219b1cd1cfa3608b5ebe
Diego Kleiman <diegokleiman@hotmail.com>
2025-05-09 14:33:43 -0500

    FAPE implementation

--- source/fape.py
+++ source/fape.py
@@ -48,5 +48,46 @@ class ReferenceFrame():
 		return self.R.apply(shifted_arr)
 
 
+def extract_backbone_coordinates(traj):
+	backbone_atoms = ['N', 'CA', 'C']
+	atom_coors = np.zeros((traj.n_residues, 3, 3))
+	for i, a in enumerate(backbone_atoms):
+		coors = traj.xyz[0, traj.top.select(f"name {a}"), :]
+		atom_coors[:, i, :] = coors
+
+	return atom_coors
 
 
+def check_rotation(rot_mat):
+	identity = np.eye(3)
+	product = np.matmul(rot_mat.transpose(0,2 ,1), rot_mat)
+	determinants = np.linalg.determinants(rot_mat)
+
+	assert np.allclose(product, identity), "Rotation matrices are not orthogonal"
+	assert np.allclose(determinants, 1.0), "Determinant may be inverted"
+
+
+def ref_frames(backbone_coords):
+	v1 = backbone_coords[:, 2] - backbone_coords[:, 1]
+	v2 = backbone_coords[:, 0] - backbone_coords[:, 1]
+	v1 /= np.linalg.norm(v1, axis=1, keepdims=True)
+	v2 -= v1*np.dot(v1, v2, axis=1)
+	v2 /= np.linalg.norm(v2, axis=1, keepdims=True)
+	assert v3.shape == (len(v1), 3)
+	rot_mat = np.stack([v1, v2, v3], axis=-1)
+	assert rot_mat.shape == (len(backbone_coords), 3, 3)
+	check_rotation(rot_mat)
+	
+	t = backbone_coords[:, 1]
+	return rot_mat, t
+
+
+def apply_rotation(coords, R, t):
+	return R @ coords + t
+
+
+def fape_squared(test_traj, ref_traj):
+	test_coords = extract_backbone_coordinates(test_traj)
+	ref_traj = extract_backbone_coordinates(ref_traj)
+
+	
